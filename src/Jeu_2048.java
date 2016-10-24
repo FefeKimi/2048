2a5a6fad1d74afebef940e46cafce5638be51872
FabriceNguy <Fabienne@User.home>
2016-10-24 14:55:25 +0200

    changement

---
+++ src/Jeu_2048.java
@@ -0,0 +1,32 @@
+
+public class Jeu_2048 {
+	
+	private void Haut(){
+		
+	}
+	public void Bas(){
+		
+	}
+	public void Gauche(){
+		
+	}
+	public void Droite(){
+		
+	}
+	public boolean Gagne(){
+		
+	}
+	public boolean Perdu(){
+		
+	}
+	public void reset(){
+		
+	}
+	public void estVide(){
+		
+	}
+	public int get(i,j){
+		return 0;
+	}
+
+}
